7eb91581e154f8c881d2ad679531e80910d3ef2e
axionneuralis-a11y <axionneuralis@gmail.com>
2026-08-11 02:07:33 +0700

    Refactor jsonResponse to use CORS headers

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,15 @@
+// utils.js
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Content-Type': 'application/json',
+};
+
 export function jsonResponse(data, status = 200) {
   return new Response(JSON.stringify(data), {
     status,
-    headers: { 'Content-Type': 'application/json' },
+    headers: CORS_HEADERS,
   });
 }
 
@@ -16,3 +24,5 @@ export function base64Encode(str) {
 export function base64Decode(str) {
   return atob(str);
 }
+
+export { CORS_HEADERS };
